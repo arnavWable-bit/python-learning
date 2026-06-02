72b7946ad3950742bd731f4dfa6b01fc69b288a1
Arnav Wable <arnavwable9@gmail.com>
2026-06-02 20:30:18 +0530

    completed day 1 python basics

--- day-1/main.py
+++ day-1/main.py
@@ -2,3 +2,23 @@
 # print("Hello World!\nHello World!")
 # print("Hello"+ " Arnav")
 # print("Hello " + input("What is your name?") + "!")
+
+# name=input("What is your name?")
+# print(name)
+
+# name="Arnav"
+# print(name)
+
+# name="upamanyu"
+# print(name)
+
+# print(len(input("What is your name?")))
+# username=input("What is your name?")
+# length=len(username)
+# print(length)
+
+# glass1="milk"
+# glass2="Juice"
+# glass1,glass2=glass2,glass1
+# print(glass1 + " "+glass2)
+
