9d2f25ebbe4954a63cb9e66b9a7a0c951ab48b45
Arnav Wable <arnavwable9@gmail.com>
2026-06-03 14:19:20 +0530

    Day 2 complete

---
+++ day-2/main.py
@@ -0,0 +1,64 @@
+#Subscripting 
+# print("Hello"[0])
+# print("Hello"[-1])
+
+#string
+# print("123" + "345") #concatenaton 
+
+#Integer
+# print(123+345)
+
+#Large numbers
+# print(123_345_567)
+# print(123345567)
+
+#Float
+#print(3.42131)
+
+#Boolean
+# print(True)
+# print(False)
+
+# len("12345")
+# print(type(123))
+# print(type("Hello"))
+
+#Type casting/ Type conversion
+# print(int("123") + int("456"))       #string -> int
+
+# print("Number of letters in your name: " + str(len(input("Enter your name: ")))) 
+
+#MATHEMATICAL OPERATIONS
+# print(123+31)
+# print(7-5)
+# print(2*3)
+# print(5/3)
+# print(5//3)
+# print(2 ** 3)
+
+
+#PEMDAS
+# ()
+# **
+# * OR /
+# + OR -
+# print(3 * 3 + 3 / 3 - 3)
+
+ 
+# bmi=84/1.65**2
+# print(bmi)
+# print(int(bmi))
+# print(round(bmi))
+# print(round(bmi,2))
+
+# score=4
+# score += 1    -= , *= , /=
+#print(score)
+
+#f-strings
+#print("your score is: " + str(score))
+# score=0
+# height=1.8
+# is_winnig= True
+# print(f"your score is = {score} , your height is = {height} , you are winning is = {is_winnig}")
+
