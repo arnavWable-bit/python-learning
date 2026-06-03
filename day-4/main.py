507b1b54efdbc53f5bbc99fa588188ca4f4f075b
Arnav Wable <arnavwable9@gmail.com>
2026-06-04 00:07:47 +0530

    day-4 complete

---
+++ day-4/main.py
@@ -0,0 +1,50 @@
+import random
+import my_module
+
+# random_number=random.randint(1,20)
+# print(random_number)
+# print(my_module.my_favourite_number)
+
+# random_number_0_to_1= random.random()
+# print(random_number_0_to_1)
+
+# random_floating_number= random.uniform()
+# print(random_floating_number)
+
+
+# HEAD OR TAIL
+# random_number= random.randint(0,1)
+# if random_number == 0 :
+#     print("Heads")
+# else :
+#     print("Tails")
+
+
+# LIST
+
+#states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+# print(states_of_america[0])
+# print(states_of_america[-1])
+
+# states_of_america.append("Arnav")
+# print(states_of_america)
+
+# states_of_america.extend(['Arnav','Upamanyu'])
+# print(states_of_america)
+
+
+# Figure out how to pick random name from the list of Friends
+#friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+# 1st option
+#print(random.choice(friends))
+#2nd option
+# random_int= random.randint(0,4)
+# print(friends[random_int])
+
+
+
+#Nested list
+fruits=["strawberries", "Apples" ,"Grapes"]
+vegetables=["Spinach","kale","Tomatoes"]
+dirty_dozen=[fruits,vegetables]
+print(dirty_dozen)
