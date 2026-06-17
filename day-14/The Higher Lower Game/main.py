cbb5f187d502929f18f9ca355963031de19a9d59
Arnav Wable <arnavwable9@gmail.com>
2026-06-17 16:08:45 +0530

    day-14 complete

---
+++ day-14/The Higher Lower Game/main.py
@@ -0,0 +1,134 @@
+import art
+from game_data import data
+import random
+import os
+
+def clear():
+    os.system("clear")
+
+print(art.logo)
+
+data_length = len(data)
+
+
+def get_random_acc():
+    index = random.randint(0,data_length - 1)
+    return data[index]
+
+
+def format_account(account):
+    return f"{account["name"]}, a {account["description"]}, from {account["country"]}"
+
+def check_answer(user_choice, followers_a, followers_b):
+    if followers_a > followers_b:
+        answer = 'a'
+    else :
+        answer = 'b'
+    
+    if user_choice == answer:
+        return True
+    else :
+        return False
+
+score = 0
+game_should_continue = True
+
+random_a = get_random_acc()
+
+while game_should_continue:
+
+    random_b = get_random_acc()
+
+    while random_a == random_b:
+        random_b = get_random_acc()
+
+
+    print(f"Compare A: {format_account(random_a)}\n")
+    print(art.vs)
+    print(f"Against B: {format_account(random_b)}\n")
+
+    compare = input("Who has more followers? Type 'A' or 'B':  ").lower()
+
+    is_user_correct = check_answer(compare, random_a["follower_count"], random_b["follower_count"] )
+
+    if is_user_correct:
+        score += 1
+        clear()
+        print(art.logo)
+        print(f"You're right! Current score: {score}")
+        random_a = random_b
+
+    else:
+        clear()
+        print(art.logo)
+        print(f"Sorry, that's wrong. Final score: {score}")
+        game_should_continue = False
+
+
+
+
+
+# # Display art
+# from art import logo, vs
+# from game_data import data
+# import random
+
+
+# def format_data(account):
+#     """Takes the account data and returns the printable format."""
+#     account_name = account["name"]
+#     account_descr = account["description"]
+#     account_country = account["country"]
+#     return f"{account_name}, a {account_descr}, from {account_country}"
+
+
+# def check_answer(a_followers, b_followers):
+#     """Take a user's guess and the follower counts and returns if they got it right."""
+#     if a_followers > b_followers:
+#         return guess == "a"
+#     else:
+#         return guess == "b"
+
+
+# print(logo)
+# score = 0
+# game_should_continue = True
+# # Generate a random account from the game data
+# account_b = random.choice(data)
+
+# # Make the game repeatable.
+# while game_should_continue:
+
+#     # Making account at position B become the next account at position A.
+#     account_a = account_b
+#     account_b = random.choice(data)
+
+#     if account_a == account_b:
+#         account_b = random.choice(data)
+
+#     print(f"Compare A: {format_data(account_a)}.")
+#     print(vs)
+#     print(f"Against B: {format_data(account_b)}.")
+
+#     # Ask user for a guess.
+#     guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+#     # Clear the screen
+#     print("\n" * 20)
+#     print(logo)
+
+#     # - Get follower count of each account
+#     a_follower_count = account_a["follower_count"]
+#     b_follower_count = account_b["follower_count"]
+
+#     # Check if user is correct.
+#     is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+#     # Give user feedback on their guess.
+#     # score keeping.
+#     if is_correct:
+#         score += 1
+#         print(f"You're right! Current score {score}")
+#     else:
+#         print(f"Sorry, that's wrong. Final score: {score}.")
+#         game_should_continue = False
