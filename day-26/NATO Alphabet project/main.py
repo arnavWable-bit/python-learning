cb9cb033dc368732331e6b05dd463c1d3015f148
Arnav Wable <arnavwable9@gmail.com>
2026-07-10 23:47:15 +0530

    feat(day-30): complete exception handling exercises and enhance password manager

--- day-26/NATO Alphabet project/main.py
+++ day-26/NATO Alphabet project/main.py
@@ -25,13 +25,34 @@ import pandas as pd
 
 #TODO 1. Create a dictionary in this format:
 
+# data = pd.read_csv("day-26/NATO Alphabet project/nato_phonetic_alphabet.csv")
+
+# phonetic_dict = {row.letter:row.code for (index,row) in data.iterrows()}
+# # print(new_dict)
+
+# #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
+# word = input("Enter a word: ").upper()
+# output_list = [phonetic_dict[letter] for letter in word]
+# print(output_list)
+
+
+
+
+# MODIFYING ON DAY-30
+
 data = pd.read_csv("day-26/NATO Alphabet project/nato_phonetic_alphabet.csv")
 
 phonetic_dict = {row.letter:row.code for (index,row) in data.iterrows()}
-# print(new_dict)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry,only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+generate_phonetic()
--- day-29/Password Manager Project/main.py
+++ day-29/Password Manager Project/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
@@ -40,22 +41,73 @@ def generate_password():
     pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+# def save():
+#     web = website_entry.get()
+#     Mail = mail_entry.get()
+#     Pass = password_entry.get()
+    
+#     if len(web)==0 or len(Pass) == 0:
+#         messagebox.showinfo(title="Oops", message="Please don’t leave any fields empty!")
+#     else:
+#         is_ok = messagebox.askokcancel(title= web, message= f"These are the details entered: \nEmail: {Mail}"
+#                                     f"\nPassword: {Pass}\n Is it ok to save?")
+        
+#         if is_ok:
+#             with open("day-29/data.txt", mode='a') as file:
+#                 file.write(f"{web} | {Mail} | {Pass} \n")
+#                 website_entry.delete(0, END) 
+#                 password_entry.delete(0, END)
+            
+            
+# MODIFIED ON DAY-30
 def save():
-    web = website_entry.get()
+    website = website_entry.get().lower()
     Mail = mail_entry.get()
     Pass = password_entry.get()
+    new_data = {
+        website: {
+            "email" : Mail,
+            "password" : Pass,
+        }
+    }
     
-    if len(web)==0 or len(Pass) == 0:
+    if len(website)==0 or len(Pass) == 0:
         messagebox.showinfo(title="Oops", message="Please don’t leave any fields empty!")
     else:
-        is_ok = messagebox.askokcancel(title= web, message= f"These are the details entered: \nEmail: {Mail}"
-                                    f"\nPassword: {Pass}\n Is it ok to save?")
-        
-        if is_ok:
-            with open("day-29/data.txt", mode='a') as file:
-                file.write(f"{web} | {Mail} | {Pass} \n")
-                website_entry.delete(0, END) 
-                password_entry.delete(0, END)
+        try:
+            with open("day-29/data.json", mode='r') as file:
+                # json.dump(new_data, file, indent=4)         # mode = 'w'   # write 
+                # Reading old data
+                data = json.load(file)                      # mode = 'r'   # read 
+                # print(data)
+                # Updating old data with new data
+        except FileNotFoundError: 
+            with open("day-29/data.json", mode='w') as file:
+                json.dump(new_data, file, indent=4)
+        else:
+            data.update(new_data)     
+            with open("day-29/data.json", mode='w') as file:
+                # Saving old data
+                json.dump(data, file, indent=4)
+        finally:
+            website_entry.delete(0, END) 
+            password_entry.delete(0, END)
+            
+# ---------------------------- SEARCH ------------------------------- #
+def find_password():
+    website = website_entry.get().lower()
+    try:
+        with open("day-29/data.json", "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showerror(title='Error', message="No Data File Found")
+    else:
+        if website in data:
+            password = data[website]['password']
+            mail = data[website]['email']
+            messagebox.showinfo(title= f"{website}", message= f"Email: {mail} \nPassword: {password}")   
+        else:
+            messagebox.showerror(title='Error', message= f"No details for the {website} exists")
             
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -68,6 +120,7 @@ lock_img = PhotoImage(file="day-29/Password Manager Project/logo.png")
 canvas.create_image(100,100, image= lock_img)
 canvas.grid(column=1, row=0)
 
+# Labels
 website_label = Label(text="Website:",font=("Arial", 18))
 website_label.grid(column=0, row=1)
 
@@ -77,14 +130,19 @@ mail_label.grid(column=0, row=2)
 password_label = Label(text="Password:",font=("Arial", 18))
 password_label.grid(column=0, row=3)
 
+# Buttons
 add_button = Button(text='Add', width=36, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
+
+search_button = Button(text="Search", width=13, command=find_password)
+search_button.grid(column=2, row=1)
 
 generate_button = Button(text='Generate Password', command=generate_password)
 generate_button.grid(column=2, row=3)
 
-website_entry = Entry(width=35)
-website_entry.grid(column=1, row=1, columnspan=2)
+# Entries
+website_entry = Entry(width=21)
+website_entry.grid(column=1, row=1)     
 website_entry.focus()
 
 mail_entry = Entry(width=35)
